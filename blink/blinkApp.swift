b1822f50f1cea6f552c0709f84ad7fc2928a1cdd
Baily Case <bailycase99@gmail.com>
2023-03-09 18:10:23 -0600

    Initial Commit

---
+++ blink/blinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  blinkApp.swift
+//  blink
+//
+//  Created by Baily Case on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct blinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
